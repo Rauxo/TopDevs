d9bb10e0bf0f39f9d57c83a432133db537dffd66
Rauxo <rahulsaikia682@gmail.com>
2026-03-24 16:13:23 +0530

    Design Home Page

--- frontend/src/pages/home/HomeScree.jsx
+++ frontend/src/pages/home/HomeScree.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import Layout from "../../Layout/Layout"
+import React from "react";
+import Layout from "../../Layout/Layout";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 function HomeScree() {
   return (
-    <Layout>HomeScree</Layout>
-  )
+    <Layout>
+      <div className="HomeContainer">
+        <div className="container-animation">
+          <div className="HomeAnimation">
+          <DotLottieReact src="/PC Coding and Dislay app Mobile.json" autoplay loop className="HomeAnimationSource"/>
+        </div>
+        </div>
+        <div className="secondContainer">
+          <h1>Welcome to TopDevs Community</h1>
+          <h2>Learn and get Hired</h2>
+          <div className="getStarted">
+            <button>Get Started </button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-export default HomeScree
+export default HomeScree;
